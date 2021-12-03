172858ae30d89b518ea9a28c90f8ec87ce4d4d10
Pavel Biryukov <eqb-pavel@yandex.com>
2021-12-03 14:10:49 +1000

    Animation & fix
    Add animation for cards.
    Fix scroll jumps on modals window.

--- dist/script.js
+++ dist/script.js
@@ -210,7 +210,8 @@ const modals = () => {
     const trigger = document.querySelectorAll(triggerSelector),
           modal = document.querySelector(modalSelector),
           close = document.querySelector(closeSelector),
-          windows = document.querySelectorAll("[data-modal]");
+          windows = document.querySelectorAll("[data-modal]"),
+          scroll = calcScroll();
     trigger.forEach(item => {
       item.addEventListener("click", e => {
         if (e.targer) {
@@ -221,7 +222,8 @@ const modals = () => {
           item.style.display = "none";
         });
         modal.style.display = "block";
-        document.body.style.overflow = "hidden"; // document.body.classList.add("modal-open");
+        document.body.style.overflow = "hidden";
+        document.body.style.marginRight = `${scroll}px`; // document.body.classList.add("modal-open");
       });
     });
     close.addEventListener("click", e => {
@@ -229,7 +231,8 @@ const modals = () => {
         item.style.display = "none";
       });
       modal.style.display = "none";
-      document.body.style.overflow = ""; // document.body.classList.remove("modal-open");
+      document.body.style.overflow = "";
+      document.body.style.marginRight = `0px`; // document.body.classList.remove("modal-open");
     });
     modal.addEventListener("click", e => {
       if (e.target === modal && closeClickOverlay) {
@@ -237,7 +240,8 @@ const modals = () => {
           item.style.display = "none";
         });
         modal.style.display = "none";
-        document.body.style.overflow = ""; //   document.body.classList.remove("modal-open");
+        document.body.style.overflow = "";
+        document.body.style.marginRight = `0px`; //   document.body.classList.remove("modal-open");
       }
     });
   }
@@ -247,6 +251,18 @@ const modals = () => {
       document.querySelector(selector).style.display = "block";
       document.body.style.overflow = "hidden";
     }, time);
+  }
+
+  function calcScroll() {
+    let div = document.createElement("div");
+    div.style.width = "50px";
+    div.style.height = "50px";
+    div.style.overflowY = "scroll";
+    div.style.visibility = "hidden";
+    document.body.appendChild(div);
+    let scrollWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+    return scrollWidth;
   }
 
   bindModal(".popup_engineer_btn", ".popup_engineer", ".popup_engineer .popup_close");
