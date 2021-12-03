075b002d8ae0872454a0cbf71c649728cc59b64e
Pavel <eqb-pavel@yandex.com>
2021-12-03 13:26:19 +1000

    init commit

---
+++ src/js/modules/forms.js
@@ -0,0 +1,63 @@
+import checkNumInputs from "./checkNumInputs";
+
+const forms = (state) => {
+  const form = document.querySelectorAll("form"),
+    inputs = document.querySelectorAll("input");
+
+  checkNumInputs("input[name='user_phone']");
+
+  const messages = {
+    loading: "Загрузка...",
+    success: "Спасибо! Скоро с Вами свяжутся",
+    error: "Что-то пошло не так...",
+  };
+
+  const postData = async (url, data) => {
+    document.querySelector(".status").textContent = messages.loading;
+    let res = await fetch(url, {
+      method: "POST",
+      body: data,
+    });
+
+    return await res.text();
+  };
+
+  const clearInputs = () => {
+    inputs.forEach((input) => {
+      input.value = "";
+    });
+  };
+
+  form.forEach((item) => {
+    item.addEventListener("submit", (event) => {
+      event.preventDefault();
+      let statusMessage = document.createElement("div");
+      statusMessage.classList.add("status");
+      item.appendChild(statusMessage);
+
+      const formData = new FormData(item);
+      if (item.getAttribute("data-calc") === "end") {
+        for (let key in state) {
+          formData.append(key, state[key]);
+        }
+      }
+
+      postData("assets/server.php", formData)
+        .then((res) => {
+          console.log(res);
+          statusMessage.textContent = messages.success;
+        })
+        .catch(() => {
+          statusMessage.textContent = messages.error;
+        })
+        .finally(() => {
+          clearInputs();
+          setTimeout(() => {
+            statusMessage.remove();
+          }, 5000);
+        });
+    });
+  });
+};
+
+export default forms;
